8293d0d351b1d2f44baf7ae86fbff46c2c977053
ibrahim oktay <ibrahimoktay@yahoo.com>
2020-08-12 09:35:53 +0300

    create new project

---
+++ BnV/BnV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BnV
+//
+//  Created by ibrahim oktay on 12.08.2020.
+//  Copyright © 2020 ibrahim oktay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
