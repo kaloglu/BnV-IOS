fef05d4dae179ac4cf3ea8edfc785ed90c80ab19
ibrahim oktay <ibrahimoktay@yahoo.com>
2020-08-13 16:04:21 +0300

    Add firebaseUI & remove googleSignIn pod

--- BnV/BnV/AppDelegate.swift
+++ BnV/BnV/AppDelegate.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
+import FirebaseCore
+import FirebaseAuth
+import FirebaseUI
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var authUI: FUIAuth!
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application
+        FirebaseApp.configure()
+//        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+//        GIDSignIn.sharedInstance().delegate = self
+        
+        authUI = FUIAuth.defaultAuthUI()
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI.delegate = self
+        let providers: [FUIAuthProvider] = [
+          FUIGoogleAuth(),
+//          FUIFacebookAuth(),
+//          FUIPhoneAuth(authUI:FUIAuth.defaultAuthUI()!),
+        ]
+        authUI.providers = providers
+        
         return true
     }
 
@@ -32,6 +49,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+          return true
+        }
+        // other URL handling goes here.
+        return false
+    }
 }
-
+//
+//extension AppDelegate: GIDSignInDelegate {
+//
+//    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+//      if let error = error {
+//        return
+//      }
+//
+//      guard let authentication = user.authentication else { return }
+//      let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+//                                                        accessToken: authentication.accessToken)
+//        print("login...")
+//        print(credential)
+//    }
+//
+//    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+//        print("Disconnected...")
+//    }
+//}
+extension AppDelegate: FUIAuthDelegate {
+    
+}
--- BnV/BnV/ViewController.swift
+++ BnV/BnV/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import FirebaseAuth
+import GoogleSignIn
 
 class ViewController: UIViewController {
-
+    
+    var handle: AuthStateDidChangeListenerHandle!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        GIDSignIn.sharedInstance()?.presentingViewController = self
+//
+//        var googleSignIn = GIDSignInButton()
+//        googleSignIn.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(googleSignIn)
+//
+//        googleSignIn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        googleSignIn.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//        googleSignIn.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+//        googleSignIn.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        
+       
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+                handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            print(user)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let authUI = (UIApplication.shared.delegate as! AppDelegate).authUI
+        let vc = authUI?.authViewController()
+        self.present(vc!, animated: false)
+    }
 }
 
